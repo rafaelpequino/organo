5908aed07bfd3ff87c5a7b1674aa2f70bbd4aac2
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-01-30 20:26:03 -0300

    Cria component Formulario

---
+++ src/components/Formulario/index.js
@@ -0,0 +1,16 @@
+import './Formulario.css';
+import CampoTexto from '../CampoTexto/';
+
+const Formulario = () => {
+    return (
+        <section>
+            <form>
+                <CampoTexto label='Nome' placeholder='Digite seu nome' />
+                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+                <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+            </form>
+        </section>
+    )
+}
+
+export default Formulario;
