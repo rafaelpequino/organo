a3860063d7dd7d414762acc270eac58346770c76
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-01-30 20:57:51 -0300

    Cria o component Formulario

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -3,8 +3,9 @@ import CampoTexto from '../CampoTexto/';
 
 const Formulario = () => {
     return (
-        <section>
+        <section className='formulario'>
             <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label='Nome' placeholder='Digite seu nome' />
                 <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
                 <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
