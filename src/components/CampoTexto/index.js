8130311197cfdb3bc6ebd5c246a58890eee9b8e7
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-07 00:13:48 -0300

    Torna o formulário responsivo

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+    
     return (
         <div className="campo-texto">
             <label>{ props.label }</label>
-            <input type='text' placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} placeholder={props.placeholder}/>
         </div>
     )
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,8 +2,10 @@ import './Formulario.css';
 import CampoTexto from '../CampoTexto/';
 import ListaSuspensa from '../ListaSuspensa/';
 import Botao from '../Botao/';
+import { useState } from 'react';
 
 const Formulario = () => {
+
     const times = [
         'Programação',
         'Front-End',
@@ -14,6 +16,11 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+
     const aoSalvar = (e) => {
         e.preventDefault();
     }
@@ -22,10 +29,34 @@ const Formulario = () => {
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label='Nome' placeholder='Digite seu nome' />
-                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
-                <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
-                <ListaSuspensa label='Time' itens={times} />
+                <CampoTexto 
+                    label='Nome'   
+                    placeholder='Digite seu nome' 
+                    valor={nome} 
+                    aoAlterado={valor => setNome(valor)} 
+                />
+
+                <CampoTexto 
+                    label='Cargo' 
+                    placeholder='Digite seu cargo' 
+                    valor={cargo} 
+                    aoAlterado={valor => setCargo(valor)} 
+                />
+
+                <CampoTexto 
+                    label='Imagem' 
+                    placeholder='Digite o endereço da imagem' 
+                    valor={imagem} 
+                    aoAlterado={valor => setImagem(valor)} 
+                />
+
+                <ListaSuspensa 
+                    label='Time' 
+                    itens={times} 
+                    valor={time} 
+                    aoAlterado={valor => setTime(valor)} 
+                />
+
                 <Botao>
                     Criar card
                 </Botao>
