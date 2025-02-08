ecbc008910baded104e61071fd15c69caa0cd4d7
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-08 09:41:54 -0300

    Define lista de colaboradores

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from '../ListaSuspensa/';
 import Botao from '../Botao/';
 import { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -23,6 +23,12 @@ const Formulario = () => {
 
     const aoSalvar = (e) => {
         e.preventDefault();
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
