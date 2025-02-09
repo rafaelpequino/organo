d45c0597fa8ea478a582a9610dbd429b6869f3fd
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-09 18:54:27 -0300

    Cria lista de times

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ import Time from './components/Time/';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#d9f7e9',
+      corSecundaria: '#57c278'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#e8f8ff',
+      corSecundaria: '#82cffa'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#f0f8e2',
+      corSecundaria: '#a6d157'
+    },
+    {
+      nome: 'DevOps',
+      corPrimaria: '#fde4e5',
+      corSecundaria: '#e06b69'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#eae9f5',
+      corSecundaria: '#db6ebf'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#fff5d9',
+      corSecundaria: '#ffba05'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ffeeff',
+      corSecundaria: '#ffba29'
+    },
+  ]
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
