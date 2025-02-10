57626b7b186026a20aebca0a5d61897dd0a46d3b
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-10 09:59:02 -0300

    Adiciona component Colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/rafaelpequino.png' alt='Rafael Pequino' />
+            </div>
+            <div>
+                <h4>Rafael Pequino</h4>
+                <h5>Instrutor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,11 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
     return (
         <section className="time" style={{ backgroundColor: props.corPrimaria }}>
             <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
+            <Colaborador />
         </section>
     )
 }
