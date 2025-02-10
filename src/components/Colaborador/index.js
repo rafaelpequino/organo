64a660078235349c1be51dfe3dda8b8eaf1f69c8
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-10 10:11:49 -0300

    Passa as devidas props para os cards de colaborador

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css';
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/rafaelpequino.png' alt='Rafael Pequino' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Rafael Pequino</h4>
-                <h5>Instrutor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -5,7 +5,7 @@ const Time = (props) => {
     return (
         <section className="time" style={{ backgroundColor: props.corPrimaria }}>
             <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
-            <Colaborador />
+            {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
         </section>
     )
 }
